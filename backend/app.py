e8573bf348a82a34c59c7419a523d1d5c5dfcb5d
Himanshu Bhatraj <himanshubhatraj888@gmail.com>
2025-07-13 12:04:37 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -122,4 +122,4 @@ def home():
     return "✅ Stratify Backend is Live and Running on Railway!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
